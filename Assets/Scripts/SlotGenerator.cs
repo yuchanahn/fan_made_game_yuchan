82978b447bb1a65d9a30c9ad0d939b1a09ca6d33
JaeJoonSim <104071482+JaeJoonSim@users.noreply.github.com>
2023-12-01 15:14:38 +0900

    Update SlotGenerator.cs

--- Assets/Scripts/SlotGenerator.cs
+++ Assets/Scripts/SlotGenerator.cs
@@ -26,41 +26,55 @@ public class SlotGenerator : MonoBehaviour
 
     private void Start()
     {
+        if (!ValidatePrefab())
+        {
+            return;
+        }
+
         GenerateSlots();
     }
 
-    private void GenerateSlots()
+    private bool ValidatePrefab()
     {
         if (slotPrefab == null)
         {
             Debug.LogError("슬롯 프리팹이 설정되지 않았습니다.");
-            return;
+            return false;
         }
+        return true;
+    }
 
-        Vector3 parentPosition = this.transform.position; // 부모 오브젝트의 위치
+    private void GenerateSlots()
+    {
+        Vector3 parentPosition = this.transform.position;
 
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
             {
-                // 슬롯의 실제 위치 계산 (부모 오브젝트 위치 포함)
-                Vector3 position = new Vector3(x * slotSpacingX, y * slotSpacingY, 0) + parentPosition;
-
-                // 슬롯 인스턴스 생성
-                GameObject newSlot = Instantiate(slotPrefab, position, Quaternion.identity);
-                newSlot.transform.parent = this.transform; // 생성된 슬롯을 현재 객체의 자식으로 설정
-
-                // 슬롯 컴포넌트 가져오기
-                Slot slot = newSlot.GetComponent<Slot>();
-
-                if (slot == null)
-                {
-                    Debug.LogError("슬롯 컴포넌트가 없습니다.");
-                    return;
-                }
-
-                slots.Add(slot);
+                CreateSlot(x, y, parentPosition);
             }
         }
+    }
+
+    private void CreateSlot(int x, int y, Vector3 parentPosition)
+    {
+        Vector3 position = CalculateSlotPosition(x, y, parentPosition);
+        GameObject newSlotObject = Instantiate(slotPrefab, position, Quaternion.identity);
+        newSlotObject.transform.parent = this.transform;
+
+        Slot slotComponent = newSlotObject.GetComponent<Slot>();
+        if (slotComponent == null)
+        {
+            Debug.LogWarning("생성된 슬롯에 Slot 컴포넌트가 없습니다: " + newSlotObject.name);
+            return;
+        }
+
+        slots.Add(slotComponent);
+    }
+
+    private Vector3 CalculateSlotPosition(int x, int y, Vector3 parentPosition)
+    {
+        return new Vector3(x * slotSpacingX, y * slotSpacingY, 0) + parentPosition;
     }
 }
